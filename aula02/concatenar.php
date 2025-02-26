b7d0c47e4ae5036c787b9430c49973281c11a028
João Pedro Miyazaki <miyazakjao120@gmail.com>
2025-02-26 10:28:29 -0300

    Aula sobre concatenação de váriaveis PHP

---
+++ aula02/concatenar.php
@@ -0,0 +1,13 @@
+<?php
+    ECHO "ola garela do youtube";
+    
+    $nome = "joão";
+    $sobrenome = "miyazaki";
+    $numero = 69;
+
+    echo "${nome} <h1> ${sobrenome} <h1/>";
+    echo $numero . "<p> " . $nome .
+
+    " " . $sobrenome . "</p>";
+
+    ?>
